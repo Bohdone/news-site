81b432e3e84ce23712d946470558837dbc045bbf
CyberMent <beatsbydoctordre1@gmail.com>
2023-11-22 16:28:39 +0200

    add 404, loader

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,12 +1,13 @@
-import React, {Suspense} from 'react';
-import {Route, Routes} from "react-router-dom";
-import {routeConfig} from "shared/config/routeConfig/routeConfig";
+import React, { Suspense } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { routeConfig } from 'shared/config/routeConfig/routeConfig'
+import { PageLoader } from 'widgets/PageLoader/PageLoader'
 
 const AppRouter = () => {
-    return (
-        <Suspense fallback={<div>Loading...</div>}>
+  return (
+        <Suspense fallback={<PageLoader/>}>
             <Routes>
-                {Object.values(routeConfig).map(({path, element}) =>
+                {Object.values(routeConfig).map(({ path, element }) =>
                     <Route
                         key={path}
                         path={path}
@@ -19,7 +20,7 @@ const AppRouter = () => {
                 )}
             </Routes>
         </Suspense>
-    );
-};
+  )
+}
 
-export default AppRouter;
+export default AppRouter
